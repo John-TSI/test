723715d196e19289374526359142d52b092acb4f
John Roughley <jroughley@softwareinstitute.com>
2022-03-24 16:44:30 +0000

    added a feature function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 
+int feature1()
+{
+    std:: cout << "This is a great feature" << std::endl;
+    return 0;
+}
+
+
 int main()
 {
-
     std:: cout << "Hello" << std::endl;
-
-
     return 0;
 }
