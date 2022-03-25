84d423e7cf16c169b82fb22d7ad774ed3c8e1028
John Roughley <jroughley@softwareinstitute.com>
2022-03-25 15:03:12 +0000

    more development

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 
+int mainBranchDevelopment()
+{
+    std:: cout << "This is more development" << std::endl;
+    return 0;
+}
+
+
 int newFeatureFromMainBranch()
 {
     std:: cout << "This is a great feature" << std::endl;
     return 0;
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 int feature1()
 {
     std:: cout << "This is a great feature" << std::endl;
